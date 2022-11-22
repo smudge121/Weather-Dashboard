d6d595c28dead3662a85b0bf56f76e732be366e7
Jishii <33364545+smudge121@users.noreply.github.com>
2022-11-21 18:14:26 -0800

    added basic HTML structure w/ Bootstrap

---
+++ script.js
@@ -0,0 +1,8 @@
+var APIkey = '9d63bca39552839c3c79e05256a07216'
+
+var inputButton = document.querySelector('button')
+var inputText = document.querySelector('input')
+
+inputButton.addEventListener('click', function(event){
+    console.log(inputText.value);
+})
