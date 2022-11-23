fd5a0681953ec6df16f74a791ecb0bb4e0838a3c
Jishii <33364545+smudge121@users.noreply.github.com>
2022-11-22 20:19:34 -0800

    publish website

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Weather-Dashboard
+
+## Description
+
+In this project I created a website that will give the user a 5 day weather forecast.  The website takes a city input.  The site will then locate the city's coordinates and then find the weather for that set of coordinates.  The website will save the user's recent searches for quick access in the future.
+
+## Usage
+
+Visit the website at this link: 
--- script.js
+++ script.js
@@ -20,9 +20,15 @@ for (var i = 0; i < cities.length; i++)
 inputButton.addEventListener('click', function(event){
     event.target.blur();
 
-    fetch(getGeo.replace('CITY', inputText.value)).then(function(response){
+    fetch(getGeo.replace('CITY', inputText.value)).then(function(response){  // Generate weather based on text input
         return response.json();
     }).then(function(data){
+        if (!data[0])
+        {
+            alert('Please enter a valid city name');
+            return;
+        }
+
         cities.push(data[0].name);
         AddToHistory(data[0].name);
         localStorage.setItem('cities',JSON.stringify(cities));
@@ -32,12 +38,12 @@ inputButton.addEventListener('click', function(event){
     });
 })
 
-function GetWeather(url, cityName)
+function GetWeather(url, cityName)  
 {
     fetch(url).then(function(response){
         return response.json();
     }).then(function(data){
-        
+
         GenerateToday(cityName, data.list);
         GenerateFiveDay(cityName, data.list);
     });
@@ -49,7 +55,7 @@ function AddToHistory(cityName){
     hist.setAttribute('class', 'btn btn-secondary col-12 m-2');
     hist.textContent = cityName;
 
-    hist.addEventListener('click', function(event){
+    hist.addEventListener('click', function(event){  // Generate weather forecast when history button is selected
         event.target.blur();
         fetch(getGeo.replace('CITY', cityName)).then(function(response){
             return response.json();
@@ -63,7 +69,7 @@ function AddToHistory(cityName){
     searchHistory.appendChild(hist);
 }
 
-function GenerateToday(cityName, weather)
+function GenerateToday(cityName, weather)  // parameters: string of city name and weather array data
 {
     mainContainer.innerHTML = '';
 
@@ -71,23 +77,23 @@ function GenerateToday(cityName, weather)
     icon.setAttribute('src', iconURL.replace('ICON',weather[0].weather[0].icon));
 
     var nameText = document.createElement('h2');
-    nameText.innerHTML += cityName + " (" + dayjs().format('DD/MM/YYYY') + ")" 
+    nameText.innerHTML += cityName + " (" + dayjs().format('DD/MM/YYYY') + ")"  //Generate City name plus date plus weather icon
         + "<img src =" + iconURL.replace('ICON',weather[0].weather[0].icon) + ">";
     nameText.setAttribute('class', 'm-2');
     mainContainer.appendChild(nameText);
 
 
-    var tempText = document.createElement('p');
+    var tempText = document.createElement('p');  // Temperature
     tempText.textContent = 'Temperature: ' + weather[0].main.temp + ' °F';
     tempText.setAttribute('class', 'm-2');
     mainContainer.appendChild(tempText);
 
-    var windText = document.createElement('p');
+    var windText = document.createElement('p');  // wind Speed
     windText.textContent = 'Wind: ' + weather[0].wind.speed + ' mph';
     windText.setAttribute('class', 'm-2');
     mainContainer.appendChild(windText);
 
-    var humText = document.createElement('p');
+    var humText = document.createElement('p');  // humididty
     humText.textContent = 'Humidity: ' + weather[0].main.humidity + '%';
     humText.setAttribute('class', 'm-2');
     mainContainer.appendChild(humText);
@@ -102,39 +108,40 @@ function GenerateFiveDay(cityName, weatherArray) {
     title.textContent = "5 Day Forecast: "
     cardContainer.appendChild(title);
 
-    for (var i = 0; i < weatherArray.length ; i++){
-        if (weatherArray[i].dt_txt.substring(11,13) == "12")
+    for (var i = 0; i < weatherArray.length ; i++){  
+        if (weatherArray[i].dt_txt.substring(11,13) == "12")  // only write a card for times of 12 o clock
         {
             WriteDateCard(weatherArray[i]);
         }
     }
 }
 
-function WriteDateCard(weatherObj){
+
+function WriteDateCard(weatherObj){ // Function to write each card in the 5 day forecast
     
     var card = document.createElement('div');
     card.setAttribute('class', 'card col m-2');
 
-    var dateText = document.createElement('h4');
+    var dateText = document.createElement('h4');  // date
     dateText.textContent = weatherObj.dt_txt.substring(0,10);
     dateText.setAttribute('class', 'm-2');
     card.appendChild(dateText);
 
-    var imgHolder = document.createElement('p');
+    var imgHolder = document.createElement('p');  // icon
     imgHolder.innerHTML = "<img src=" + iconURL.replace('ICON',weatherObj.weather[0].icon) + ">";
     card.appendChild(imgHolder);
 
-    var tempText = document.createElement('p');
+    var tempText = document.createElement('p'); // temperature
     tempText.textContent = 'Temp: ' + weatherObj.main.temp + ' °F';
     tempText.setAttribute('class', 'm-2');
     card.appendChild(tempText);
 
-    var windText = document.createElement('p');
+    var windText = document.createElement('p'); // wind speed
     windText.textContent = 'Wind: ' + weatherObj.wind.speed + ' mph';
     windText.setAttribute('class', 'm-2');
     card.appendChild(windText);
 
-    var humText = document.createElement('p');
+    var humText = document.createElement('p');  // humidity
     humText.textContent = 'Humidity: ' + weatherObj.main.humidity + '%';
     humText.setAttribute('class', 'm-2');
     card.appendChild(humText);
